27d73b810375c3526f4fa6ec9e99ce331437ea60
pop_os_laptop <49096264+niallsiow@users.noreply.github.com>
2024-10-12 16:14:14 +0100

    Simple server - accepts basic get, responds with basic response

---
+++ httpserver.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+
+encoding_standard = 'ISO-8859-1'
+
+if len(sys.argv) > 2:
+    print('usage: webserver.py <port>')
+
+port = 28333
+if len(sys.argv) == 2:
+    port = int(sys.argv[1])
+
+print(sys.argv)
+print(port)
+
+response = 'HTTP/1.1 200 OK\n'
+response += 'Content-Type: text/plain\n'
+response += 'Content-Length: 6\n'
+response += 'Connection: close\n'
+response += '\n'
+response += 'Hello!'
+
+response = response.encode(encoding_standard)
+
+s = socket.socket()
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind(('', port))
+
+s.listen()
+
+while(1):
+    new_conn = s.accept()
+    new_socket = new_conn[0]
+
+    request = new_socket.recv(4096).decode(encoding_standard)
+    while '\r\n\r\n' not in request:
+        request += new_socket.recv(4096).decode(encoding_standard)
+    
+    new_socket.sendall(response)
+    new_socket.close()
